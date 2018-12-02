d0d265b3ca220a7679b8bd5dbce8f1a14dcd0f78
Alex Kosov <kosov.av@phystech.edu>
2018-12-03 00:36:23 +0300

    Added invocation of FibonacciHierarchy

--- homeworks/hw2/main.cpp
+++ homeworks/hw2/main.cpp
@@ -1,30 +1,12 @@
 #include <iostream>
+#include "scatter_hierarchy.h"
 #include "fibonacci_hierarchy.h"
 
 int main() {
-//    std::cout << FibonacciNumber<92>::value << std::endl;
     std::cout << FibonacciNumber<92>::value << std::endl;
     using list = TypeList<int, char, float, double, float, std::string>;
     GenLinearHierarchy<list, Unit> linear_hierarchy;
-    GenScatterHierarchy<list, Holder> scatter_hierarchy;
+//    GenScatterHierarchy<list, Holder> scatter_hierarchy;
+    GenFibonacciHierarchy<list, Unit> fibonacci_hierarchy;
     return 0;
 }
-
-
-//int main() {
-//    using list = TypeList<int, double, char>;
-//    using empty_list = TypeList<>;
-//    std::cout << Length<list>::value << " " << Length<empty_list>::value << std::endl;
-//    std::cout << std::is_same<TypeAt<2, list>::type, char>::value << " " <<
-//              std::is_same<TypeAt<1, list>::type, int>::value << std::endl;
-////    std::cout << std::is_same<TypeAt<1, empty_list>::type, int>::value << std::endl; // static assert failed
-//    std::cout << list() << std::endl;
-//    std::cout << AddToBeginning<void *, list>::type_list() << std::endl;
-////    std::cout << list() << std::endl; // list hasn't changed
-//    std::cout << Insert<3, float, list>::type_list() << std::endl;
-//    std::cout << RemoveFrom<0, list>::type_list() << RemoveFrom<1, list>::type_list()
-//              << RemoveFrom<2, list>::type_list() << std::endl;
-//    std::cout << Replace<1, float, list>::type_list() << std::endl;
-//    std::cout << "Hello, World!" << std::endl;
-//    return 0;
-//}
